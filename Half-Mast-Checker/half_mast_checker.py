99ca4c4e97aacf76e10eddfbcd77b65559f0b816
Henry Hall <henrydh@byu.edu>
2022-03-29 17:40:23 -0600

    Added sending capabilities Some clean up in the main function.

--- Half-Mast-Checker/half_mast_checker.py
+++ Half-Mast-Checker/half_mast_checker.py
@@ -1,8 +1,32 @@
 # I'm going to use this to check if the flag is at half mast every day, and if it is, to alert me
+from audioop import add
 import time
 import ezgmail
 import requests
 from pathlib import Path
+
+def get_addresses():
+    try:
+        address_file = Path('addresses.txt')
+        addresses = address_file.read_text()
+        addresses = addresses.splitlines()
+    except:
+        print('***It seems there is not an addresses file. Please add it!***')
+    if len(addresses) < 1:
+        raise ValueError('No addresses.')
+    return addresses
+
+def send_status_email(text_to_send):
+    ezgmail.init()
+    addresses = get_addresses()
+    for receiver in addresses:
+        ezgmail.send(receiver, 'Today\'s Flag Status', text_to_send)
+    pass
+
+def get_national_status():
+    my_text = get_stars_and_stripes()
+    national_flag_status = get_stars_and_stripes_status(my_text)
+    return national_flag_status
 
 def get_stars_and_stripes():
     # Get the page from SSD make it text.
@@ -39,7 +63,7 @@ def get_status_line(text_to_search):
 
 def get_utah_status():
     utah_stat = 'none'
-    utah_stat = requests.get('https://governor.utah.gov/flag-status/') # Use this to get the SSD page
+    utah_stat = requests.get('https://governor.utah.gov/flag-status/') # Use this to get the Utah flat status page
     utah_text = utah_stat.text
     for line in utah_text.split('\n'):
         if 'The flag of the United States of America and the flag of the state of Utah are currently at' \
@@ -51,17 +75,27 @@ def get_utah_status():
     return utah_stat
 
 if __name__ == '__main__':
+    # Initialize email message
+    email_report = ''
     # Set a default status.
     national_flag_status = 'none'
-    my_text = get_stars_and_stripes()
-    national_flag_status = get_stars_and_stripes_status(my_text)
+    utah_stat = 'none'
 
+    # Get the statuses
+    national_flag_status = get_national_status()
     utah_stat = get_utah_status()
 
-    print(f'National Status: {national_flag_status}')
-    print(f'Utah Status: {utah_stat}')
-    
-    ezgmail.init()
+    # Test print to cmd
+    #print(f'National Status: {national_flag_status}')
+    #print(f'Utah Status: {utah_stat}')
+
+    # Assemble email message.
+    email_report = email_report + 'Today\'s Flag Status: \n' + f'National Status: {national_flag_status}' \
+        + '\n' + f'Utah Status: {utah_stat}' + '\n' \
+            + 'For more info see https://starsandstripesdaily.org/ and https://governor.utah.gov/flag-status/'
+
+    #print(email_report)
+    send_status_email(email_report)
 
     # Send status.
     # TODO: Send status.
