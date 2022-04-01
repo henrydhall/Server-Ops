22857f78620e5cad92362308f51e01a0bbb5d9f9
Henry Hall <henrydh@byu.edu>
2022-03-31 19:51:18 -0600

    Ready to go. Cleaned up code. Put all of the 'main' function into a new function that is now just called. Made it so the email sending function can return a string or send the email.

--- Half-Mast-Checker/half_mast_checker.py
+++ Half-Mast-Checker/half_mast_checker.py
@@ -1,48 +1,75 @@
 # I'm going to use this to check if the flag is at half mast every day, and if it is, to alert me
-from audioop import add
-import time
+
+# Dependencies this program uses.
 import ezgmail
 import requests
+import os
 from pathlib import Path
 
 def get_addresses():
+    """
+    # returns: list of strings.
+    # Used to get the addresses. Read line by line from addresses.txt
+    # The user will need to add addresses to the file themselves.
+    """
     try:
         address_file = Path('addresses.txt')
         addresses = address_file.read_text()
         addresses = addresses.splitlines()
     except:
-        print('***It seems there is not an addresses file. Please add it!***')
+        raise ValueError('***It seems there is not an addresses file. Please add it!***')
     if len(addresses) < 1:
         raise ValueError('No addresses.')
     return addresses
 
 def send_status_email(text_to_send):
-    ezgmail.init()
+    # You'll need to change this to whatever the correct directory is.
+    try:
+        os.chdir(r'/home/henry/Git Folders/Server-Ops-Git/Half-Mast-Checker/')
+    except:
+        raise SystemError('Directory not found.')
+    try:
+        ezgmail.init()
+    except:
+        raise SystemError('Problem with token, check other errors.')
+    
     addresses = get_addresses()
+
+    # Send to each receiver one by one.
     for receiver in addresses:
         ezgmail.send(receiver, 'Today\'s Flag Status', text_to_send)
-    pass
 
 def get_national_status():
-    my_text = get_stars_and_stripes()
-    national_flag_status = get_stars_and_stripes_status(my_text)
+    """
+    # Returns strings 'FULL STAFF' or 'HALF STAFF' depending on stars and stripes daily.
+    """
+    stars_and_stripes_text = get_stars_and_stripes()
+    national_flag_status = get_stars_and_stripes_status(stars_and_stripes_text)
     return national_flag_status
 
 def get_stars_and_stripes():
     # Get the page from SSD make it text.
-    stars_and_stripes = requests.get('https://starsandstripesdaily.org/') # Use this to get the SSD page
-    my_text = stars_and_stripes.text
-    return my_text
+    stars_and_stripes_page = requests.get('https://starsandstripesdaily.org/') # Use this to get the SSD page
+    stars_and_stripes_text = stars_and_stripes_page.text # Get the page as text.
+    return stars_and_stripes_text
 
-def get_stars_and_stripes_status(string_to_check):
+def get_stars_and_stripes_status(page_as_text):
+    """
+    # Parameters: 
+    #   page_as_text: the stars and stripes website page as text.
+    # Returns:
+    #   flag_status: 'HALF STAFF' or 'FULL STAFF' depending on ssd
+    """
+
+    # Initialize the flag status as none.
     flag_status = 'none'
 
-    # Process text, get what I believe is the status line.
-    my_text = string_to_check.split('\n')
+    # Process text, get the status line.
+    my_text = page_as_text.split('\n')
     for i in range(0,len(my_text)):
         my_text[i] = my_text[i].strip()
     if '<h3>The status of the American Flag today is</h3>' in my_text:
-        flag_status = get_status_line(my_text)
+        flag_status = get_starsandstripes_status_line(my_text)
     else:
         flag_status = 'unable to get status'
 
@@ -54,7 +81,8 @@ def get_stars_and_stripes_status(string_to_check):
 
     return flag_status
 
-def get_status_line(text_to_search):
+def get_starsandstripes_status_line(text_to_search):
+    # Finds the status line based on the line that should be right before it.
     previous_line = '<h3>The status of the American Flag today is</h3>'
     line_number = text_to_search.index(previous_line)
     status_line = line_number + 1
@@ -74,7 +102,10 @@ def get_utah_status():
                 utah_stat = 'HALF STAFF'
     return utah_stat
 
-if __name__ == '__main__':
+def email_flag_status(send_email = True):
+    """
+    # Using this to clean up the code. Does everything to compile and send the email.
+    """
     # Initialize email message
     email_report = ''
     # Set a default status.
@@ -85,18 +116,14 @@ if __name__ == '__main__':
     national_flag_status = get_national_status()
     utah_stat = get_utah_status()
 
-    # Test print to cmd
-    #print(f'National Status: {national_flag_status}')
-    #print(f'Utah Status: {utah_stat}')
-
     # Assemble email message.
     email_report = email_report + 'Today\'s Flag Status: \n' + f'National Status: {national_flag_status}' \
         + '\n' + f'Utah Status: {utah_stat}' + '\n' \
             + 'For more info see https://starsandstripesdaily.org/ and https://governor.utah.gov/flag-status/'
+    if send_email == True:
+        send_status_email(email_report)
+    else:
+        return email_report
 
-    #print(email_report)
-    send_status_email(email_report)
-
-    # Send status.
-    # TODO: Send status.
-    # TODO: Run regularly.
+if __name__ == '__main__':
+    email_flag_status(True)
